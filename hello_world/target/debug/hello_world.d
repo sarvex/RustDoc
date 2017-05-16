85e85df2c8dea41fe9fcf2c1830739e67eba7090
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-05-16 11:59:04 +0530

    Intial Commit

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/sarvex/Workspace/RustDoc/hello_world/target/debug/hello_world: /Users/sarvex/Workspace/RustDoc/hello_world/src/main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/sarvex/Workspace/RustDoc/hello_world/target/release/hello_world: /Users/sarvex/Workspace/RustDoc/hello_world/src/main.rs
